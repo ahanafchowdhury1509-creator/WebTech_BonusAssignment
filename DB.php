6726fe682487c2d3627718c7ee14dcd25a9219d7
SyedAhanaf Chowdhury Auditta <ahanafchowdhury1509@gmail.com>
2026-01-30 02:39:10 +0600

    The Database Connection File.

---
+++ DB.php
@@ -0,0 +1,23 @@
+<?php
+$host="localhost";
+$user="root";
+$db="product_db";
+function Connect(){
+    $global $host;
+    $global $user;
+    $global $db;
+    $con=mysqli_connect($host,$user,"",$db);
+    if(!con){
+        die("DB Connection Failed: " . mysqli_connect_error());
+    }
+    return  $con;
+}
+
+
+
+
+
+
+
+
+?>
